97144836386f72290f60d30141e7a6163dfc07dc
M Aghead Albakhe <aghyadalbalkhi@gmail.com>
2020-08-26 12:32:30 +0300

    Add Validation and fuction and some script code

--- javascript.js
+++ javascript.js
@@ -1,18 +1,34 @@
 
-var customerName;
+var customerName = prompt("Please Enter Your Name");
 var address; 
 var phone;
 var order;
-var NoItems;
 var pagelink;
 var pagename;
+
+
+//Name Validtion 
+if (customerName==null || customerName==""){
+   alert("Name can't be blank");  
+   customerName = prompt("Please Enter Your Name");
+}
+
+// Print The Name In the Top Of Page 
+document.write('<h2> Weclome '+customerName+' </h2>');
+   
+
 function myFunction() { 
     
-   customerName = prompt("Please Enter Your Name");
+   
    address = prompt("Please Enter Your Address");
    phone = prompt("Please Enter Your Phone");
-   order = prompt("Please Enter Your choice form list bleow /n 1 - Food Time  /n 2- Damsucs Food 3- Hut Food");
-
+   order = prompt("Please Enter Your choice the number form list bleow  1 - Food Time  2- Damsucs Food 3- Hut Food");
+  
+   //Order Validtion 
+   if (order!==1 || order!==3){
+      alert("order Must be a Number");  
+      order = prompt("Please Enter Number of Order");
+   
    if (order==1){
 
     pagelink='https://joudi12.github.io/lab04/';
@@ -27,6 +43,8 @@ function myFunction() {
     pagelink='https://aghyadalbalkhi-asac.github.io/Lab-04---Structure-web-pages-with-HTML/';
     pagename='Damsucs Food';
    }
+
+}
 console.log(pagelink);
 
 document.getElementById("IDOFELEMENT").href = pagelink;
